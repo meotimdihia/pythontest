abe2cc53488560679a51a9bcc5b7b1b3d83f7f6f
Dien <meotimdihia@gmail.com>
2014-05-02 00:04:40 +0700

    update

--- heo_temp_device.py
+++ heo_temp_device.py
@@ -1,47 +1,25 @@
 #!/usr/bin/python           # This is server.py file
 
-
-
 import socket               # Import socket module
-
 from time import sleep
 
 s = socket.socket()         # Create a socket object
-
 #host = socket.gethostname() # Get local machine name
-
 host ='123.30.53.72'
-
 port = 8222               # Reserve a port for your service.
-
 s.bind((host, port))        # Bind to the port
-
-
-
 s.listen(5)                 # Now wait for client connection.
-
 #c, addr = s.accept()     # Establish connection with client.
-
 #print 'Got connection from', addr
-
-
 
 while True:
 
-       c, addr = s.accept()     # Establish connection with client.
-
-          print 'Got connection from', addr
-
-             c.send('READ')
-
-                print c.recv(1024)
-
-                 #  sleep(1)
-
-                  #  c.send('READ')
-
-                    # print c.recv(1024)
-
-                       sleep(3)
-
-                          c.close()                # Close the connection
+	c, addr = s.accept()     # Establish connection with client.
+	print 'Got connection from', addr
+	c.send('READ')
+	print c.recv(1024)
+	#  sleep(1)
+	#  c.send('READ')
+	# print c.recv(1024)
+	sleep(3)
+	c.close()                # Close the connection
